3b56c4ac4b4750f7d818a795d5c8a5af4ddc1e2d
NanakShrestha <nanakshrestha777@gmail.com>
2024-07-09 21:58:18 +0545

    trying a model for prediction

--- regression.py
+++ regression.py
@@ -23,28 +23,42 @@ df.fillna(-99999, inplace = True)
 # ceil rounds up to the nearest int | math.ceil(2.3)~3
 # floor rounds down to the nearest int  | math.floor(2.7)~2
 forecast_out = int(math.ceil(0.01*len(df)))
-
+print(forecast_out)
 df['label'] = df[forecast_col].shift(-forecast_out)
 
 df.dropna(inplace=True)
 print(df.head())
 
 
-x = np.array(df.drop(['label'],axis = 1))
+X = np.array(df.drop(['label'],axis = 1))
 y = np.array(df['label'])
-
-
-
 # Print lengths before adjustment
-print("Before adjustment:", len(x), len(y)) 
+# print("Before adjustment:", len(x), len(y)) 
 
-x = preprocessing.scale(x)
-x = x[:-forecast_out+1]
-
+X = preprocessing.scale(X)
+# x = X[:-forecast_out+1]
 # df.dropna(inplace = True)
 y = np.array(df['label'])
 
 # Print lengths after adjustment
-print(len(x), len(y))
+print(len(X), len(y))
 
 
+#Training and Testing sets
+
+
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
+
+#testing svm algorithm
+
+clf = svm.SVR(kernel='poly')
+clf.fit(X_train, y_train)
+accuracy_svm = clf.score(X_test, y_test)
+
+print(accuracy_svm)
